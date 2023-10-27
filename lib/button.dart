f7e4ec7b31c2a6b65f82b08e35d1cf120baf750e
aimeerimeminova <aimemynoo@gmail.com>
2023-10-27 14:53:02 +0600

    Main

---
+++ lib/button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, this.color});
+
+  final color;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
